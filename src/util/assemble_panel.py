7743572bc5b4b5557ea3dbca654e7f320bfe92eb
Encephala <jonathan_rietveld@hotmail.com>
2025-01-17 12:02:48 +0100

    Make importable, sadly no longer executable

--- src/util/assemble_panel.py
+++ src/util/assemble_panel.py
@@ -3,11 +3,12 @@
 from pathlib import Path
 import pandas as pd
 
-from data_load import load_df
+from .data_load import load_df
 
 # I sure hope LISS actually uses a consistent prefix for its studies
 def assemble_wide_panel(prefix: str) -> pd.DataFrame:
-    """Loads all files starting with the given prefix and merges them into a wide-form dataframe."""
+    """Loads all files starting with the given prefix and merges them into a wide-form dataframe.
+    This is done on the assumption that there are no duplicate files containing the same data."""
     result = pd.DataFrame()
 
     for file in Path("../data").glob(prefix + "*"):
@@ -19,9 +20,3 @@ def assemble_wide_panel(prefix: str) -> pd.DataFrame:
         )
 
     return result
-
-if __name__ == "__main__":
-    df = assemble_wide_panel("cp")
-
-    print(df)
-    print(df.columns)
