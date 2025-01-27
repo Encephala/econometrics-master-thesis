71b8ff7c7c2483ab1c8822ed72325338639b00d7
Encephala <jonathan_rietveld@hotmail.com>
2025-01-27 12:19:18 +0100

    More efficient overlap window, fix x_name check

--- src/util/model.py
+++ src/util/model.py
@@ -31,8 +31,6 @@ class ModelDefinitionBuilder:
         return self
 
     def with_lag_structure(self, lag_structure: list[int]) -> Self:
-        raise NotImplementedError
-
         self.lag_structure = lag_structure
         return self
 
@@ -42,27 +40,33 @@ class ModelDefinitionBuilder:
         columns = data.columns
 
         y_columns = [col for col in columns if col.startswith(self.y)]
-        y_start = min((int(column[column.find("_") + 1 :]) for column in y_columns))
-        y_end = max((int(column[column.find("_") + 1 :]) for column in y_columns))
+        y_start = min(int(column[column.find("_") + 1 :]) for column in y_columns)
+        y_end = max(int(column[column.find("_") + 1 :]) for column in y_columns)
 
         x_columns = [col for col in columns if col.startswith(self.x)]
-        x_start = min((int(column[column.find("_") + 1 :]) for column in x_columns))
-        x_end = max((int(column[column.find("_") + 1 :]) for column in x_columns))
-
-        # TODO?: Yield some information on the breadth of the overlap?
-        # TODO: I can be more efficient with the data if x starts earlier than y
-        overlap_start = max(x_start, y_start)
-        overlap_end = min(x_end, y_end)
+        x_start = min(int(column[column.find("_") + 1 :]) for column in x_columns)
+        x_end = max(int(column[column.find("_") + 1 :]) for column in x_columns)
 
         # Build a regression starting max_lag years after the overlap
         lag_structure = self.lag_structure if self.lag_structure is not None else [1]
 
-        for year in range(overlap_start + max(lag_structure), overlap_end):
-            y_name = f"{self.y}_{year}"
-            x_names = [f"{self.x}_{year - lag}" for lag in lag_structure]
+        # TODO?: Yield some information on the breadth of the overlap?
+        # TODO?: When using FIML/handling missing data, perhaps the `max` and `min` in these two statements should swap,
+        # but then we have to also add those columns to the df to prevent index errors
 
-            if y_name not in columns or x_names not in columns:
-                warnings.warn(f"For {year=}, either {y_name} or {x_names} was not found", stacklevel=2)
+        # if x goes far enough back, start when y starts
+        # else, start as soon as we can
+        first_year_y = y_start if x_start + max(lag_structure) <= y_start else x_start + max(lag_structure)
+        # if x goes far enough forward, stop when x_stops
+        # else, stop when y stops
+        last_year_y = x_end + min(lag_structure) if x_end + min(lag_structure) <= y_end else y_end
+
+        for year_y in range(first_year_y, last_year_y):
+            y_name = f"{self.y}_{year_y}"
+            x_names = [f"{self.x}_{year_y - lag}" for lag in lag_structure]
+
+            if y_name not in columns or any(x_name not in columns for x_name in x_names):
+                warnings.warn(f"For {year_y=}, either {y_name} or one of {x_names} was not found", stacklevel=2)
                 continue
 
             regression = f"{y_name} ~ {x_names}\n"
