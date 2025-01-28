ba7269a1e825d18b1bd6a086a4e4716c64dda79b
Encephala <jonathan_rietveld@hotmail.com>
2025-01-28 21:18:54 +0100

    Verify SEM works with complex reverse causality

--- src/exploratory_mlsem_simulation.py
+++ src/exploratory_mlsem_simulation.py
@@ -8,25 +8,32 @@ from util import ModelDefinitionBuilder
 
 # %% data generation
 N = 500
-T = 5
+T = 7
 
 RHO = 0.42
-BETA = 0.69
+BETA0 = 0.69
+BETA1 = 0.75
+BETA2 = 0.20
+BETA4 = 1
 
 rng = np.random.default_rng()
 epsilon = rng.normal(0, np.sqrt(3), size=(N, T))
 
-# TODO: Correlate x with the previous epsilon (reverse causality)
 x = rng.normal(5, 3, size=(N, T))
+x[:, 1:] += BETA1 * np.roll(epsilon, 1, axis=1)[:, 1:]
+x[:, 2:] += BETA2 * np.roll(epsilon, 2, axis=1)[:, 2:]
+x[:, 4:] += BETA4 * np.roll(epsilon, 4, axis=1)[:, 4:]
 
 y = np.empty([N, T])
 # TODO: if y_0 is a constant, the covariance matrix becomes non-PD.
 # I think that's because I'm not properly treating y_0 as exogenous?
-# Like it shouldn't be a problem.
-y[:, 0] = rng.normal(0, 0.1, size=N)
+# Like it shouldn't be a problem?
+# Hmm when fixing the variance of y_0 to be any constant, the model still complains about non-PD'ness.
+# Curious
+y[:, 0] = rng.normal(0, 0.05, size=N)
 
 for t in range(1, T):
-    y[:, t] = RHO * y[:, t - 1] + BETA * x[:, t - 1] + epsilon[:, t]
+    y[:, t] = RHO * y[:, t - 1] + BETA1 * x[:, t - 1] + epsilon[:, t]
 
 # %% model definition
 complete_data = pd.DataFrame()
