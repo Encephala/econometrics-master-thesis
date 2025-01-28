96b684669210c83ffa7c98ca9c7c8ee29d25ef95
Encephala <jonathan_rietveld@hotmail.com>
2025-01-28 15:17:28 +0100

    Fix model definition
    Now correctly has:
    - each regression having the same paremeter values
    - each regression having the same variance in epsilon (y)
    - pre-determinedness of x

--- src/exploratory_mlsem_simulation.py
+++ src/exploratory_mlsem_simulation.py
@@ -6,23 +6,25 @@ import semopy
 
 from util import ModelDefinitionBuilder
 
-# %%
+# %% data generation
 N = 500
 T = 5
 
-x = np.array(list(range(N)) * T).reshape((N, T)) + np.random.normal(0, 2, size=(N, T))  # noqa: NPY002
-
-# %%
-RHO = 0.42
+RHO = 0
 BETA = 0.69
+
+epsilon = np.random.normal(0, 1, size=(N, T))  # noqa: NPY002
+
+# TODO: Correlate x with the previous epsilon (reverse causality)
+x = np.array(list(range(N)) * T).reshape((N, T)) + np.random.normal(0, 2, size=(N, T))  # noqa: NPY002
 
 y = np.empty((N, T))
 y[:, 0] = 0
 
 for t in range(1, T):
-    y[:, t] = RHO * y[:, t - 1] + BETA * x[:, t - 1] + np.random.normal(0, 1, size=(N))  # noqa: NPY002
+    y[:, t] = RHO * y[:, t - 1] + BETA * x[:, t - 1] + epsilon[:, t]
 
-# %%
+# %% model definition
 complete_data = pd.DataFrame()
 for t in range(T):
     complete_data[f"y_{t}"] = y[:, t]
@@ -33,9 +35,11 @@ model_definition = (
     ModelDefinitionBuilder().with_x("x").with_y("y").with_lag_structure(lag_structure).build(complete_data)
 )
 
+print(f"Model definition:\n{model_definition}")
+
 model = semopy.Model(model_definition)
 
-# %%
+# %% model estimation
 optimisation_result = model.fit(complete_data)
 print(optimisation_result)
 
--- src/util/model.py
+++ src/util/model.py
@@ -54,22 +54,26 @@ class ModelDefinitionBuilder:
         # TODO?: When using FIML/handling missing data, perhaps the `max` and `min` in these two statements should swap,
         # but then we have to also add those columns to the df to prevent index errors
 
-        # if x goes far enough back, start when y starts
-        # else, start as soon as we can
+        # if x goes far enough back, the first regression is when y starts
+        # else, start as soon as we can due to x
+        # TODO: Update this so that it is correct when we include a lag structure for y
         first_year_y = max(y_start, x_start + max(lag_structure))
-        # if x goes far enough forward, stop when x_stops
+        # if x does not go far enough forward, stop when x_stops
         # else, stop when y stops
         last_year_y = min(x_end + min(lag_structure), y_end)
+        last_year_x = last_year_y - min(lag_structure)
 
-        for year_y in range(first_year_y, last_year_y):
+        for year_y in range(first_year_y, last_year_y + 1):
             y_name = f"{self.y}_{year_y}"
             x_names = [f"{self.x}_{year_y - lag}" for lag in lag_structure]
 
             if y_name not in columns or any(x_name not in columns for x_name in x_names):
-                warnings.warn(f"For {year_y=}, either {y_name} or one of {x_names} was not found", stacklevel=2)
+                warnings.warn(f"For {year_y=}, either {y_name} or one of {x_names} was not in the data", stacklevel=2)
                 continue
 
-            regression = f"{y_name} ~ {' + '.join(x_names)}"
+            rvals = [f"rho_0*{self.y}_{year_y - 1}", *(f"beta{i}*{name}" for i, name in enumerate(x_names))]
+
+            regression = f"{y_name} ~ {' + '.join(rvals)}"
             result.append(regression)
 
             if self.y_ordinal:
@@ -78,7 +82,24 @@ class ModelDefinitionBuilder:
             if self.x_ordinal:
                 result.append(f"DEFINE(ordinal) {' '.join(x_names)}")
 
-        # TODO?: implement some stuff to make it less likely that I forget to properly order an ordinal column
+            # Fix variance for y to be constant in time
+            result.append(f"{y_name} ~~ sigma*{y_name}")
+
+        # Allow for pre-determined variables, i.e. arbitrary correlation between x and previous values of y
+        # NOTE: The very first year of y in the data is considered exogenous, but first_year_y is the first regression
+        # that we do, so this range correctly starts for the first endogenous y
+        for year in range(first_year_y, last_year_y + 1):
+            y_current_name = f"{self.y}_{year}"
+            x_future_names = [f"{self.x}_{future_year}" for future_year in range(year + 1, last_year_x + 1)]
+
+            if len(x_future_names) == 0:
+                continue
+
+            operation = f"{y_current_name} ~~ {' + '.join(x_future_names)}"
+            result.append(operation)
+
+        # TODO?: implement some stuff to make it less likely that I forget to properly order an ordinal column,
+        # since imported categorical data has arbitrary category to index coding
 
         if len(result) == 0:
             warnings.warn("Result empty, no model defined", stacklevel=2)
