7251ce14e01b40cfcbcfe6d9a15af33612116b69
Encephala <jonathan_rietveld@hotmail.com>
2025-01-28 13:29:34 +0100

    Finish simple simulation

--- src/exploratory_mlsem_simulation.py
+++ src/exploratory_mlsem_simulation.py
@@ -2,12 +2,15 @@
 import pandas as pd
 import numpy as np
 
+import semopy
+
+from util import ModelDefinitionBuilder
+
 # %%
 N = 500
 T = 5
 
 x = np.array(list(range(N)) * T).reshape((N, T)) + np.random.normal(0, 2, size=(N, T))  # noqa: NPY002
-
 
 # %%
 RHO = 0.42
@@ -20,3 +23,20 @@ for t in range(1, T):
     y[:, t] = RHO * y[:, t - 1] + BETA * x[:, t - 1] + np.random.normal(0, 1, size=(N))  # noqa: NPY002
 
 # %%
+complete_data = pd.DataFrame()
+for t in range(T):
+    complete_data[f"y_{t}"] = y[:, t]
+    complete_data[f"x_{t}"] = x[:, t]
+
+lag_structure = [1]
+model_definition = (
+    ModelDefinitionBuilder().with_x("x").with_y("y").with_lag_structure(lag_structure).build(complete_data)
+)
+
+model = semopy.Model(model_definition)
+
+# %%
+optimisation_result = model.fit(complete_data)
+print(optimisation_result)
+
+model.inspect()
