75d78ee0ec0d57b9ab2d96e50b84f6f77838a25c
Encephala <jonathan_rietveld@hotmail.com>
2025-01-29 12:17:39 +0100

    Expand AR structure, standardise parameter names

--- src/exploratory_mlsem_simulation.py
+++ src/exploratory_mlsem_simulation.py
@@ -10,30 +10,37 @@ from util import ModelDefinitionBuilder
 N = 500
 T = 7
 
-RHO = 0.42
-BETA0 = 0.69
-BETA1 = 0.75
-BETA2 = 0.20
-BETA4 = 1
+ALPHA1 = 0.42
+ALPHA2 = 0.1234
+ALPHA3 = 0.4321
+BETA1 = 1.69
+GAMMA1 = 0.75
+GAMMA2 = 0.20
+GAMMA4 = 1
 
 rng = np.random.default_rng()
 epsilon = rng.normal(0, np.sqrt(3), size=(N, T))
 
 x = rng.normal(5, 3, size=(N, T))
-x[:, 1:] += BETA1 * np.roll(epsilon, 1, axis=1)[:, 1:]
-x[:, 2:] += BETA2 * np.roll(epsilon, 2, axis=1)[:, 2:]
-x[:, 4:] += BETA4 * np.roll(epsilon, 4, axis=1)[:, 4:]
+# Correlate x with previous epsilon
+x[:, 1:] += GAMMA1 * np.roll(epsilon, 1, axis=1)[:, 1:]
+x[:, 2:] += GAMMA2 * np.roll(epsilon, 2, axis=1)[:, 2:]
+x[:, 4:] += GAMMA4 * np.roll(epsilon, 4, axis=1)[:, 4:]
 
 y = np.empty([N, T])
 # TODO: if y_0 is a constant, the covariance matrix becomes non-PD.
 # I think that's because I'm not properly treating y_0 as exogenous?
 # Like it shouldn't be a problem?
-# Hmm when fixing the variance of y_0 to be any constant, the model still complains about non-PD'ness.
+# Hmm when fixing the variance of y_0 to be any constant in the model definition,
+# the fitting still complains about non-PD'ness.
 # Curious
 y[:, 0] = rng.normal(0, 0.05, size=N)
+y[:, 1] = ALPHA1 * y[:, 0] + BETA1 * x[:, 0] + epsilon[:, 1]
+y[:, 2] = ALPHA1 * y[:, 1] + ALPHA2 * y[:, 0] + BETA1 * x[:, 1] + epsilon[:, 2]
+y[:, 3] = ALPHA1 * y[:, 2] + ALPHA2 * y[:, 1] + ALPHA3 * y[:, 0] + BETA1 * x[:, 2] + epsilon[:, 3]
 
-for t in range(1, T):
-    y[:, t] = RHO * y[:, t - 1] + BETA1 * x[:, t - 1] + epsilon[:, t]
+for t in range(4, T):
+    y[:, t] = ALPHA1 * y[:, t - 1] + ALPHA2 * y[:, t - 2] + ALPHA3 * y[:, t - 3] + BETA1 * x[:, t - 1] + epsilon[:, t]
 
 # %% model definition
 complete_data = pd.DataFrame()
@@ -53,4 +60,4 @@ model = semopy.Model(model_definition)
 optimisation_result = model.fit(complete_data)
 print(optimisation_result)
 
-model.inspect()
+model.inspect().sort_values(["op", "Estimate", "lval"])  # pyright: ignore[reportOptionalMemberAccess, reportAttributeAccessIssue]
