373acd7c8142c5266235c8462b3493eaafc9c86a
Encephala <jonathan_rietveld@hotmail.com>
2025-04-07 17:25:11 +0200

    Don't fold parameter table on print

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -7,5 +7,5 @@ def print_results(model: semopy.Model):
     # https://stackoverflow.com/a/20937592/5410751
     results = model.inspect().sort_values(["op", "rval"])  # pyright: ignore noqa: PGH003
 
-    with pd.option_context("display.float_format", "{:.4f}".format):
+    with pd.option_context("display.float_format", "{:.4f}".format, "display.max_rows", None):
         display(results)
