556472c90c5b0ff72b7cb6697b1d14df9d2a34ed
Encephala <jonathan_rietveld@hotmail.com>
2025-05-13 09:28:56 +0200

    Check time series stability

---
+++ src/report/time_series_stability.py
@@ -0,0 +1,34 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+N = 1000
+
+
+# %%
+# `coefs` from most recent lag to least recent
+def empirical(coefs):
+    data = np.empty(N)
+
+    for i in range(len(coefs)):
+        data[i] = 1
+
+    for i in range(len(coefs), N):
+        data[i] = np.sum(data[i - len(coefs) : i] * np.flip(coefs))
+
+    plt.figure()
+    plt.plot(data)
+    plt.title(str(coefs))
+    plt.show()
+
+
+def analytical(coefs):
+    roots = np.roots([*np.flip(coefs), -1])
+
+    plt.figure()
+    plt.scatter(roots.real, roots.imag)
+
+    plt.gca().add_patch(Circle((0, 0), 1, fill=False))
+
+    plt.show()
