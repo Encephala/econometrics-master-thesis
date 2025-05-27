44774a63b1d3eba8bd9828ca8699990c54b9c893
Encephala <jonathan_rietveld@hotmail.com>
2025-05-27 15:37:57 +0200

    Add plot to show stable MHI5 scores

---
+++ src/report/mhi5_time_variability.py
@@ -0,0 +1,19 @@
+# %%
+import matplotlib.pyplot as plt
+
+from lib.data import available_waves, make_all_data, select_variable
+
+# ruff: noqa: F403, F405
+from lib.data.variables import *
+
+data = make_all_data(cache=True)
+mhi5 = select_variable(data, MHI5)
+means = mhi5.mean()
+
+plt.figure()
+plt.plot(list(available_waves(mhi5)), means, "-o")
+plt.title("Mean MHI5-score over time")
+plt.ylim(0, 100)
+plt.ylabel("MHI5")
+plt.xlabel("Year")
+plt.show()
