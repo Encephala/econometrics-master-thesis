de5f64b4a26ad3c6ba34594b88b05a795801d2ac
Encephala <jonathan_rietveld@hotmail.com>
2025-05-28 12:42:37 +0200

    Also check sports stability through covid

--- src/report/y_x_time_variability.py
+++ src/report/y_x_time_variability.py
@@ -7,6 +7,8 @@ from lib.data import available_waves, make_all_data, select_variable
 from lib.data.variables import *
 
 data = make_all_data(cache=True)
+
+# %% MHI5
 mhi5 = select_variable(data, MHI5)
 means = mhi5.mean()
 
@@ -26,5 +28,28 @@ plt.plot(list(available_waves(mhi5)), stds, "-o")
 plt.title("Std of MHI5-score over time")
 plt.ylim(0, 30)
 plt.ylabel(r"$\sigma_{MHI5}$")
+plt.xlabel("Year")
+plt.show()
+
+# %% Examine sports
+sports = select_variable(data, SPORTS)
+means = sports.mean()
+
+plt.figure()
+plt.plot(list(available_waves(sports)), means, "-o")
+plt.title("Sports engagement rate over time")
+plt.ylim(0, 1)
+plt.ylabel(r"$p_{sports}$")
+plt.xlabel("Year")
+plt.show()
+
+# %% Sports std
+stds = sports.std()
+
+plt.figure()
+plt.plot(list(available_waves(sports)), stds, "-o")
+plt.title("Std of MHI5-score over time")
+plt.ylim(0, 1)
+plt.ylabel(r"$\sigma_{p_{sports}}$")
 plt.xlabel("Year")
 plt.show()
