83b0b4969bcc831d6027eb110e028d643c671eb3
Encephala <jonathan_rietveld@hotmail.com>
2025-06-02 13:13:27 +0200

    Errorbar-plot of MHI5 and Sports side by side

--- src/report/y_x_time_variability.py
+++ src/report/y_x_time_variability.py
@@ -10,10 +10,10 @@ data = make_all_data(cache=True)
 
 # %% MHI5
 mhi5 = select_variable(data, MHI5)
-means = mhi5.mean()
+mhi5_means = mhi5.mean()
 
 plt.figure()
-plt.plot(list(available_waves(mhi5)), means, "-o")
+plt.plot(list(available_waves(mhi5)), mhi5_means, "-o")
 plt.title("Mean MHI5-score over time")
 plt.ylim(0, 100)
 plt.ylabel("MHI5")
@@ -21,10 +21,10 @@ plt.xlabel("Year")
 plt.show()
 
 # %% Examine std
-stds = mhi5.std()
+mhi5_stds = mhi5.std()
 
 plt.figure()
-plt.plot(list(available_waves(mhi5)), stds, "-o")
+plt.plot(list(available_waves(mhi5)), mhi5_stds, "-o")
 plt.title("Std of MHI5-score over time")
 plt.ylim(0, 30)
 plt.ylabel(r"$\sigma_{MHI5}$")
@@ -33,10 +33,10 @@ plt.show()
 
 # %% Examine sports
 sports = select_variable(data, SPORTS)
-means = sports.mean()
+sports_means = sports.mean()
 
 plt.figure()
-plt.plot(list(available_waves(sports)), means, "-o")
+plt.plot(list(available_waves(sports)), sports_means, "-o")
 plt.title("Sports engagement rate over time")
 plt.ylim(0, 1)
 plt.ylabel(r"$p_{sports}$")
@@ -44,12 +44,31 @@ plt.xlabel("Year")
 plt.show()
 
 # %% Sports std
-stds = sports.std()
+sports_stds = sports.std()
 
 plt.figure()
-plt.plot(list(available_waves(sports)), stds, "-o")
+plt.plot(list(available_waves(sports)), sports_stds, "-o")
 plt.title("Std of MHI5-score over time")
 plt.ylim(0, 1)
 plt.ylabel(r"$\sigma_{p_{sports}}$")
 plt.xlabel("Year")
+plt.show()
+
+# %% Combined plots
+figure, axes = plt.subplots(1, 2, figsize=(9, 4))
+
+axes[0].errorbar(list(available_waves(mhi5)), mhi5_means, yerr=mhi5_stds, capsize=2)
+axes[0].set_title("Mean MHI5-score over time")
+axes[0].set_ylim(0, 100)
+axes[0].set_ylabel("MHI5")
+axes[0].set_xlabel("Year")
+axes[0].set_xticks(list(range(8, 24, 2)))
+
+axes[1].errorbar(list(available_waves(sports)), sports_means, yerr=sports_stds, capsize=2)
+axes[1].set_title("Sports engagement over time")
+axes[1].set_ylabel("Rate of engagement in sports")
+axes[1].set_xlabel("Year")
+
+plt.tight_layout()
+plt.savefig("../report/thesis/figures/data/errorbar_mhi5_sports.svg")
 plt.show()
