82e677f9c3ae83e43ecb69f1feb503f1f8cff8a4
Encephala <jonathan_rietveld@hotmail.com>
2025-06-09 10:51:19 +0200

    Calculator for yearly correlation between variables

---
+++ src/report/correlation_calculator.py
@@ -0,0 +1,47 @@
+# %% imports
+import pandas as pd
+
+from lib.data import make_all_data, load_wide_panel_cached, select_variable, available_waves, select_wave
+
+# ruff: noqa: F403, F405
+from lib.data.variables import *
+
+# %%
+health = load_wide_panel_cached("ch")
+leisure = load_wide_panel_cached("cs")
+background = load_wide_panel_cached("avars")
+
+all_data = make_all_data(cache=True)
+
+
+# %%
+def print_correlations(left: pd.DataFrame, right: pd.DataFrame):
+    for wave in available_waves(pd.concat([left, right], axis=1)):
+        print(f"{wave=}")
+
+        try:
+            left_var = select_wave(left, wave)
+        except KeyError:
+            print("Left not available")
+            continue
+
+        try:
+            right_var = select_wave(right, wave)
+        except KeyError:
+            print("Right not available")
+            continue
+
+        print(pd.concat([left_var, right_var], axis=1).corr())
+
+
+# %% Disease and medication
+disease = select_variable(all_data, DISEASE_STATUS)
+medication = select_variable(health, "ch184")
+medication = medication.map(lambda x: x == "yes", na_action="ignore")
+
+print_correlations(disease, medication)
+
+# %% Disease and physical health
+physical_health = select_variable(all_data, PHYSICAL_HEALTH)
+
+print_correlations(disease, physical_health)
