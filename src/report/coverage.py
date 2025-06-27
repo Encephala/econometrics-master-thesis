dd3fdb3cbe85797ef1a99dc2cb0da200a75df6c3
Encephala <jonathan_rietveld@hotmail.com>
2025-06-27 16:03:50 +0200

    report: Fix coverage table to be after deletion of individuals

--- src/report/coverage.py
+++ src/report/coverage.py
@@ -1,10 +1,23 @@
 # %%
-from lib.data import make_all_data, available_waves, Column
+from lib.data import make_all_data, available_waves, Column, select_variable
 
 # ruff: noqa: F403, F405
 from lib.data.variables import *
 
 data = make_all_data(cache=True)
+
+# %% Remove variables as in ../path_analysis.py
+# Drop rows for which the dependent variable is always NA, as these will never be included in a regression.
+y = select_variable(data, MHI5)
+y_missing = y.isna().sum(axis=1) == y.shape[1]
+y_missing = y_missing[y_missing].index
+data = data.drop(y_missing)
+
+# Drop rows for which sports is always NA, as these provide no information towards our variable of interest
+x = select_variable(data, SPORTS)
+x_missing = x.isna().sum(axis=1) == x.shape[1]
+x_missing = x_missing[x_missing].index
+data = data.drop(x_missing)
 
 # %%
 for wave in available_waves(data):
