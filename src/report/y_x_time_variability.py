e1c959a91bfdb131a3a679dc3f2521031e13545e
Encephala <jonathan_rietveld@hotmail.com>
2025-07-14 10:43:49 +0200

    report: Remove figure titles

--- src/report/y_x_time_variability.py
+++ src/report/y_x_time_variability.py
@@ -12,11 +12,10 @@ data = make_all_data(cache=True)
 mhi5 = select_variable(data, MHI5)
 mhi5_means = mhi5.mean()
 
-mhi5_years = [f"20{year}" for year in list(available_waves(mhi5))]
+mhi5_years = [f"20{year:02d}" for year in list(available_waves(mhi5))]
 
 plt.figure()
 plt.plot(mhi5_years, mhi5_means, "-o")
-plt.title("Mean MHI5 score over time")
 plt.ylim(0, 100)
 plt.ylabel("MHI5")
 plt.xlabel("Year")
@@ -27,7 +26,6 @@ mhi5_stds = mhi5.std()
 
 plt.figure()
 plt.plot(mhi5_years, mhi5_stds, "-o")
-plt.title("Std of MHI5 score over time")
 plt.ylim(0, 30)
 plt.ylabel(r"$\sigma_{MHI5}$")
 plt.xlabel("Year")
@@ -41,7 +39,6 @@ sports_years = [f"20{year}" for year in list(available_waves(sports))]
 
 plt.figure()
 plt.plot(sports_years, sports_means, "-o")
-plt.title("Sports engagement rate over time")
 plt.ylim(0, 1)
 plt.ylabel(r"$p_{sports}$")
 plt.xlabel("Year")
@@ -52,7 +49,6 @@ sports_stds = sports.std()
 
 plt.figure()
 plt.plot(sports_years, sports_stds, "-o")
-plt.title("Std of MHI5 score over time")
 plt.ylim(0, 1)
 plt.ylabel(r"$\sigma_{p_{sports}}$")
 plt.xlabel("Year")
@@ -63,19 +59,19 @@ mhi5_no_nan = [mhi5[col].dropna() for col in mhi5]
 
 plt.figure(figsize=(4, 3))
 plt.boxplot(mhi5_no_nan, positions=list(available_waves(mhi5)), whis=(10, 90), showfliers=False)
-plt.title("MHI5 score distribution over time")
 plt.ylim(-4, 104)
 plt.ylabel("MHI5")
 plt.xlabel("Year")
-plt.xticks(list(range(8, 24, 2)), labels=list(range(8, 24, 2)))  # pyright: ignore  # noqa: PGH003
+xticks = [f"20{year:02d}" for year in range(8, 24, 2)]
+plt.xticks(list(range(8, 24, 2)), labels=xticks, rotation=90)  # pyright: ignore  # noqa: PGH003
 plt.savefig("../report/thesis/figures/data/boxplot_mhi5.svg")
 
 plt.figure(figsize=(4, 3))
 plt.plot(list(available_waves(sports)), sports_means, ".-", color="black")
-plt.title("Sports engagement over time")
 plt.ylim(0, 1)
 plt.ylabel("Rate of engagement in sports")
 plt.xlabel("Year")
+plt.xticks(list(range(8, 24, 2)), xticks, rotation=90)
 plt.savefig("../report/thesis/figures/data/errorbar_sports.svg")
 
 plt.show()
